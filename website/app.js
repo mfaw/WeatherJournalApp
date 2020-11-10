a0f1a8a944201302d6902b4c077d69f233166dbd
mfaw <64710994+mfaw@users.noreply.github.com>
2020-11-10 17:17:27 +0300

    added global variables

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
-
+let URL = "http://api.openweathermap.org/data/2.5/weather?zip="
+const apiKey = "6476141f82da5f302a7a464e6bbf9d9f"
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
