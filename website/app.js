82e440a2da9e594426259eea3b4847f689c396ae
mfaw <64710994+mfaw@users.noreply.github.com>
2020-11-10 17:26:31 +0300

    added Async GET

--- website/app.js
+++ website/app.js
@@ -4,3 +4,19 @@ const apiKey = "6476141f82da5f302a7a464e6bbf9d9f"
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+//EventListner 
+
+//Async GET
+const retrieveWeatherData = async (URL, apiKey) =>{ 
+    const request = await fetch(URL + apiKey);
+    try {
+    // Transform into JSON
+    const weatherData = await request.json()
+    return weatherData;
+    }
+    catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+  };
+  
