a055eb84290785be744ef63c834dbf8f7a6a0793
mfaw <64710994+mfaw@users.noreply.github.com>
2020-11-10 19:05:06 +0300

    chained promises to update UI

--- website/app.js
+++ website/app.js
@@ -43,3 +43,37 @@ function performAction(e){
 
 }
 
+//Async POST
+const postData = async ( url = '', data = {})=>{
+
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+
+    try {
+      const newData = await response.json();
+      return newData;
+    }catch(error) {
+    console.log("error", error);
+    }
+};
+
+//Chain to update UI
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try{
+      const allData = await request.json();
+      const length = allData.length; 
+      document.getElementById('date').innerHTML = allData[length-1].date;
+      document.getElementById('temp').innerHTML = allData[length-1].temperature;
+      document.getElementById('content').innerHTML = allData[length-1].UserResponse;
+  
+    }catch(error){
+      console.log("error", error);
+    }
+  }
